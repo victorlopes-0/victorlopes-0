c80d6df5ed1d06eff305cb07744c13be05be8c1a
Victor Lopes <victorlopes.dev@outlook.com>
2026-05-02 13:02:38 -0300

    feat: create css file

---
+++ tokens.ts
@@ -0,0 +1,8 @@
+export const tokens = {
+    colors: {
+        cyanBlue: "#00f7ff",
+        malachiteGreen: "#00cc55",
+        sugilitePurple: "#a855f7",
+        amethystPurple: "#6366f1",
+    },
+} as const;
